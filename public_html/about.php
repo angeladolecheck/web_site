ca419d7a66b87752cabbdc904da9f230ae7d181c
angeladolecheck <31734522+angeladolecheck@users.noreply.github.com>
2017-11-02 08:46:05 -0500

    php

---
+++ public_html/about.php
@@ -0,0 +1,60 @@
+<!--Angela Dolecheck-->
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Pop Up Games Mini Golf</title>
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/sandstone/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+
+<style>
+    h2 {
+        text-align:center;
+        font-size: 35px;
+        font-family: Engravers MT;
+        border-bottom: solid #078fe8;
+        border-top: solid #078fe8;
+        padding: 30px 0px 30px 0px;
+    }
+</style>
+
+</head>
+
+
+
+<body>
+
+
+<?php include 'navigation.php'; ?>
+
+<div class = "below_menu_container">
+
+
+
+
+<h1 style="text-align:center;font-size: 35px;font-family: Engravers MT;
+border-bottom: solid #078fe8;border-top: solid #078fe8;padding: 30px 0px 30px 0px;"> Pop Up Games</h1>
+
+  <img src="img/logo.jpg" alt="Pop Up Games Logo"/>
+  
+
+<h2>Mission Statement</h2>
+
+
+  <p>At Pop Up Games we strive to add entertainment to your event through an easy and affordable rental experience.</p>
+
+
+  <h2>Traditions</h2>
+  
+  <p>We want to have many lasting traditions starting with the events we participated in already like the Pi Phi 50th Celebration.</p>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+  <?php include 'footer.php'; ?>
